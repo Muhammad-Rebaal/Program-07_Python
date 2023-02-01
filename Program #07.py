0e95f26e3b99dfd63632013da5d13edb6a13ed9e
Muhammad-Rebaal <122050424+Muhammad-Rebaal@users.noreply.github.com>
2023-02-01 13:36:35 -0800

    Add files via upload

---
+++ Program #07.py
@@ -0,0 +1,10 @@
+height = float(input("Enter the height of triangle : "))
+base = float(input("Enter the base of triangle : "))
+def area():
+    return "The area of triangle is : "+ str((0.5)*height*base)
+
+print (area())
+
+
+a_area = height*base*0.5
+print("The area of triangle is :",a_area)
